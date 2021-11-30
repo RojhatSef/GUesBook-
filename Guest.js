fda00eca90c58434ba3bffe2a33d570df5853076
RojhatSefdin <Rojhat10@hotmail.com>
2021-11-30 16:12:46 +0100

    json wall

--- Guest.js
+++ Guest.js
@@ -7,7 +7,7 @@ console.log("Kör servern på localhost:3000");
 app.use(express.static("public"));
 let fs = require("fs"); 
 const guest = (JSON.parse(fs.readFileSync('guestbook.json')));
-
+console.log(guest); 
 app.get("/form", (req, res) => {
     res.sendFile(__dirname + "/form.html"); // liknande som ovan
 });
@@ -19,11 +19,16 @@ app.post("/skriva-fil", (req, res) => {
     name: req.body.nameInput,
     family: req.body.familyInput,
     phoneInput: req.body.phoneInput,
-    email: req.body.emailInput}
+    email: req.body.email}
    guest.push(newUser); 
    console.log(guest); 
    fs.writeFileSync('guestbook.json', JSON.stringify(guest, null, 4))
+   res.redirect('/skapa'); 
  });
+ app.get('/skapa', function (req, res) {   // request sidan när de vill komma åt /skapa
+
+    res.send(guest); // annars tillbaks till inloggning sidan.
+});
 
 
   
