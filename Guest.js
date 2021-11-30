f2b5648b36430b09a00b6723b4a12e83f2ef1f7c
RojhatSefdin <Rojhat10@hotmail.com>
2021-11-30 09:16:02 +0100

    Testing more things

--- Guest.js
+++ Guest.js
@@ -1,2 +1,16 @@
-window.onload(); 
+// window.onload(); 
 
+const http = require("http");
+const express = require("express"); 
+const app = http.createServer(function(req,res){
+    res.end("Hejsan NodeJS!"); 
+}); 
+app.listen(3000)
+console.log("Kör servern på localhost 3000")
+
+let fs = require("fs"); 
+app.get("/besokare", (req, res) => {
+    fs.readFile("besokare.txt", (err, data) => {
+        
+    })
+})
